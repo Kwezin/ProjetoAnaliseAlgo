9e9941bf8e32db56b0e219b72bc809e31a210e22
Kauê Henrique Matias Alves <91916789+Kwezin@users.noreply.github.com>
2024-09-05 13:58:54 -0300

    Update jdoodle (1).c

--- jdoodle (1).c
+++ jdoodle (1).c
@@ -3,21 +3,29 @@
 #include <stdio.h>    // Para perror e printf
 
 bool segmento(int seq[8], int seg[3]){
-    int i, j, count_true = 0;
+    int i, j;
     
     for(i=0; i<8; i++){
        if(seg[0] == seq[i]){
-           
-           for(j=0; j<3 ; j++){
-               if(seg[j] == seq[i]){
+           int aux = i+1;
+           int count_true = 1;
+           for(j=1; j<3 ; j++){
+               if(seg[j] == seq[aux]){
                    count_true++;
+                   aux++;
                    
+               }else{
+                   break;
                }
-               i++;
+               
+           }
+           
+           if(j==3){ // n
+               return true;
            }
        }
     }
-    return count_true == 3;
+    return false;
 }
 int main(){
     int sequencia[8] = {1, 2, 3, 4, 5, 6, 7, 8};
